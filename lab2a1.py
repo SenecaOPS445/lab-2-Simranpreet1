14dd5b54fd68b18296feeae8377af21d88091ea8
Simranpreet1 <simranpreet-kaur14@myseneca.ca>
2024-06-08 19:47:49 -0400

    Update lab2a1.py

--- lab2a1.py
+++ lab2a1.py
@@ -2,7 +2,7 @@
 
 name = 'Jon'
 age = 20
-# print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
 
 colour = input("Type in a colour and press enter: ")
 print('The colour I typed in is: ' + colour)
