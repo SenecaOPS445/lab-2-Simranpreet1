a0997ba914d0c9b171e3f11efd1377ff6d5e4902
Simranpreet1 <simranpreet-kaur14@myseneca.ca>
2024-06-08 19:43:45 -0400

    Update lab2g.py

--- lab2g.py
+++ lab2g.py
@@ -6,13 +6,13 @@
 
 import sys
 
-# Check if an argument was provided and set the timer accordingly
+
 if len(sys.argv) > 1:
     timer = int(sys.argv[1])
 else:
     timer = 3
 
-# Loop to count down and print the timer
+
 while timer > 0:
     print(timer)
     timer -= 1
